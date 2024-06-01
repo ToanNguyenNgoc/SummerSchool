67e0157d4a4d1e02dffe4edaded54b7c8ec0205c
Nguyen Ngoc Toan <ngoctoan06011998@gmail.com>
2024-06-01 07:38:34 +0700

    change config

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,33 @@
-export default () => ({});
+export default () => ({
+  documentation: {
+    enabled: true,
+    config: {
+      openapi: '3.0.0',
+      info: {
+        version: '1.0.1',
+        title: 'SUMMER SCHOOL DOCUMENTATION',
+        description: '',
+        termsOfService: 'YOUR_TERMS_OF_SERVICE_URL',
+        // contact: {
+        //   name: 'TEAM',
+        //   email: 'contact-email@something.io',
+        //   url: 'mywebsite.io'
+        // },
+        license: {
+          name: 'Apache 2.0',
+          url: 'https://www.apache.org/licenses/LICENSE-2.0.html'
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:1337/api",
+          description: "Development server"
+        },
+        {
+          url: "https://summerschool.fashional.pro/api",
+          description: "Live server"
+        },
+      ],
+    },
+  }
+});
